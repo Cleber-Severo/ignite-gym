0b260598cb8ec27de050e6028967bb3aa6e9ad85
Cleber Severo <clebersevero1998@gmail.com>
2025-10-16 21:42:54 -0300

    chore: add refresh token logic

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -35,12 +35,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   async function storageUserAndTokenSave(
     userData: UserDTO,
-    token: string
+    token: string,
+    refresh_token: string
   ): Promise<void> {
     try {
       setIsLoadingUserStorageData(true);
       await storageUserSave(userData);
-      await storageAuthTokenSave(token);
+      await storageAuthTokenSave({ token, refresh_token });
     } catch (error) {
       throw error;
     } finally {
@@ -53,8 +54,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true);
       const { data } = await api.post('/sessions', { email, password });
 
-      if (data.user && data.token) {
-        await storageUserAndTokenSave(data.user, data.token);
+      if (data.user && data.token && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token);
         userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
@@ -91,7 +92,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       setIsLoadingUserStorageData(true);
       const loggedUser = await storageUserGet();
-      const token = await storageAuthTokenGet();
+      const { token } = await storageAuthTokenGet();
 
       if (token && loggedUser) {
         setUser(loggedUser);
@@ -107,6 +108,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   useEffect(() => {
     loadUserData();
   }, []);
+
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut);
+
+    return () => {
+      subscribe();
+    };
+  }, [signOut]);
 
   return (
     <AuthContext.Provider
